af24c8a635ad2a7b6a987d2bee9ea0174f09d520
narmanguebraun <narmanguebraun1@gmail.com>
2022-04-27 15:43:59 +0200

    add first folders and files, first routes

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const authRoutes = requite('./routes/auth.routes');
+
 const app = express();
+
+app.use(authRoutes);
 
 app.listen(3000);
---
+++ controllers/auth.controller.js
@@ -0,0 +1,14 @@
+function getSignUp(req, res) {
+
+}
+
+function getLogIn(req, res) {
+
+}
+
+module.exports = {
+  getSignUp: getSignUp,
+  getLogIn: getLogIn
+};
+
+

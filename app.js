0764bfe5818e2aa8e9bb756379ffcbf65266196c
narmanguebraun <narmanguebraun1@gmail.com>
2022-04-27 18:30:38 +0200

    add ejs and first views

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const authRoutes = requite('./routes/auth.routes');
 
 const app = express();
+// Tell express to use ejs to render views and where to find my views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(authRoutes);
 

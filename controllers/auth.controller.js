c1d271386602315a7598ffb2e1f002db0d4ced8a
narmanguebraun <narmanguebraun1@gmail.com>
2022-05-06 13:58:53 +0200

    add User signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,26 @@
+const User = require('../models/user.model');
+
 function getSignUp(req, res) {
   res.render('customer/auth/signup');
 }
 
-function signup(req, res) {
-  
+async function signup(req, res) {
+  const user = new User(
+    req.body.email, 
+    req.body.password, 
+    req.body.fullname, 
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  await user.signup();
+
+  res.redirect('/login');
 }
 
 function getLogIn(req, res) {
-
+  res.render('customer/auth/login')
 }
 
 module.exports = {
